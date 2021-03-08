57088801a5f7f4160503ba549e2deab744968c19
HugoS <hugo@postcolour.com>
2021-03-09 00:12:39 +0800

    Update pdf_converter.py

--- pdf_converter.py
+++ pdf_converter.py
@@ -47,10 +47,12 @@ try:
 
     st.write(df_new_clean_column)
 
-    csv = df_new_clean_column.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-    st.markdown(href, unsafe_allow_html=True)
+    ragic_version = st.checkbox("CSV For Ragic版本")
+    if ragic_version:
+        csv = df_new_clean_column.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+        st.markdown(href, unsafe_allow_html=True)
 except AttributeError:
     st.write("選擇上傳公告")
 
