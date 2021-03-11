48d047dc277488a0e3156964c87f609943e9accd
HugoS <hugo@postcolour.com>
2021-03-12 00:54:49 +0800

    update

--- src/pages/pdf_converter.py
+++ src/pages/pdf_converter.py
@@ -7,10 +7,7 @@ import pandas as pd
 import pdfplumber
 import numpy as np
 
-# def file_selector(folder_path='.'):
-#     filenames = os.listdir(folder_path)
-#     selected_filename = st.selectbox('Select a file', filenames)
-#     return os.path.join(folder_path, selected_filename)
+
 def write():
     st.title('PDF to Excel Converter')
 
